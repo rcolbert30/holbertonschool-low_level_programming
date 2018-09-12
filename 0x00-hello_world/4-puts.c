d5b2f664dbb27eff79817fd354594da53e38fcb2
shaadibo <colbertrashaad@gmail.com>
2018-09-12 20:01:13 +0000

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/** main - prints a string
+/**
+ * main - prints a string
  *
- * Return: 0 on success 
+ * Return: 0 on success
  */
 
-int main (void)
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
