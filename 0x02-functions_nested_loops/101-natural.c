6edbb719cbfb73ac0f8df6a40f9b0d48eb9f5c0f
shaadibo <colbertrashaad@gmail.com>
2018-09-19 00:42:50 +0000

    untracked

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lidt natural numbers below 10 that are multiples of 3 and 6
+ * computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: 0
+*/
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 15 == 0)
+		{
+			a += b;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int t1 = 1, t2 = 2, nextTerm = 0, n;
+
+	nextTerm = t1 + t2;
+
+	for (n = 3; n <= 50; n++)
+	{
+		printf("%d, ",nextTerm);
+		t1 = t2;
+	 	t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	return 0;
+}
