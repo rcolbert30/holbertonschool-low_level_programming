2f50f95b3244ac88a216a44bad3cfaf432680b24
shaadibo <colbertrashaad@gmail.com>
2018-09-19 17:29:03 +0000

    more fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+nclude <stdio.h>
+
+// Function to print first n Fibonacci Numbers 
+void printFibonacciNumbers(int n)
+{
+    int f1 = 0, f2 = 1, i;
+
+    if (n < 1)
+        return;
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d, ", f2);
+        int next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+    }
+}
+int main()
+{
+    printFibonacciNumbers(98);
+    return 0;
+}
