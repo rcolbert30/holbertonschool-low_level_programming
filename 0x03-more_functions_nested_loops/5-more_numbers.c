8a167bc1fd5126b7aff10b7b3c0e1e3bc10e5178
shaadibo <colbertrashaad@gmail.com>
2018-09-20 19:41:37 +0000

    more numbers

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,14 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10, a++)
+	for (a = 0; a <= 10; a++)
 	{
-		for (b = '0'; b <= '14'; b++)			
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
-		}		
-	}
+			if (b > 9)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
 	_putchar('\n');
+	}
 }
