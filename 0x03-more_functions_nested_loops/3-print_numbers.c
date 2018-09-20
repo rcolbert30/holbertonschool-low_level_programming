9168a91288a4cb677edf45c03e8c3e06ab67654a
shaadibo <colbertrashaad@gmail.com>
2018-09-20 19:08:15 +0000

    print numbers

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_numbers - prints 0 - 9
+ * Return: 0
+*/
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
