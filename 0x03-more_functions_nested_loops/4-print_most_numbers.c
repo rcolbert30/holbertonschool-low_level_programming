f3e4882f2455f72ed078cc34e5a752a7f9e8d2a1
shaadibo <colbertrashaad@gmail.com>
2018-09-20 19:12:51 +0000

    print most numbers

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_numbers - prints 0 - 9
+ * Return: 0
+*/
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != 4)
+		_putchar(a);
+	}
+	_putchar('\n');
+}
