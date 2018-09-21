139142f15b657b6e6bcd555ac68e1b42de4b1143
shaadibo <colbertrashaad@gmail.com>
2018-09-21 18:01:58 +0000

    print triangle:

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: size of the triangle
+ * Return: 0
+*/
+void print_triangle(int size)
+{
+	int margin, dist, i;
+
+	for (i = 1; i <= size; i++)
+	{
+		margin = size - i;
+	while (margin--)
+	{
+		_putchar(' ');
+	}
+	dist = i;
+	while (dist--)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+	}
+}
