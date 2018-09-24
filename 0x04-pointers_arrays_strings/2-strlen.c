9d367f15561ba3dba709db0dc110f5378aca885b
shaadibo <colbertrashaad@gmail.com>
2018-09-24 18:12:20 +0000

    string length

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int string;
+
+	for (string = 0; string[s] != '\0'; string++)
+	{
+		;
+	}
+	return (string);
+}
