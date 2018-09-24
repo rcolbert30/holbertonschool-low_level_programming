425084b8af1042fe557b3c2d8d88f5fb2ff4bcbf
shaadibo <colbertrashaad@gmail.com>
2018-09-24 18:51:40 +0000

    print reverse:

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @a: string
+ * Return: string length
+*/
+int _strlen(char *a)
+{
+	int string;
+
+	for (string = 0; string[a] != '\0'; string++)
+	{
+		;
+	}
+	return (string);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * Return: reverse string
+*/
+void print_rev(char *s)
+{
+	int reverse;
+
+	for (reverse = _strlen(s) - 1; s[reverse] >= 0; reverse--)
+	{
+		_putchar(s[reverse]);
+	}
+	_putchar('\n');
+}
