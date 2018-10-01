f8fada2911d948ef333526e166c417499963e03e
shaadibo <colbertrashaad@gmail.com>
2018-10-01 19:37:02 +0000

    adding files

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		src[counter] = dest[counter];
+	}
+	return (dest);
+}
