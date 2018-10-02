55262735370f4aa9c503060ab037b40899b9167f
shaadibo <colbertrashaad@gmail.com>
2018-10-02 05:54:43 +0000

    set string:

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_diagsums - prints a square of sums
+ * @a: pointer
+ * @size: size of square
+ * Return: 0
+*/
+void print_diagsums(int *a, int size)
+{
+	int kd;
+
+	for (kd = 0; a[kd] < size; kd++)
+		;
+}
