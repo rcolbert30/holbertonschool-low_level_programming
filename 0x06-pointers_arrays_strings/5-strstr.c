dc59ffc93fd2225aa939484e07e340a4e3fa3f9c
shaadibo <colbertrashaad@gmail.com>
2018-10-02 05:33:44 +0000

    strstr

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to beginning of located substring
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int kd;
+	char *ptr;
+
+	kd = 0;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		ptr = haystack;
+		while (*ptr == needle[kd])
+		{
+			kd++;
+			ptr++;
+			if (*needle == '\0' || needle[kd] == '\0')
+				return (haystack);
+		}
+	}
+	return (0);
+}
+	
