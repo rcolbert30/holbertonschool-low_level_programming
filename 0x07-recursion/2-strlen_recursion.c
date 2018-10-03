b2f12aace2ef35666784d649f8d798100015a570
shaadibo <colbertrashaad@gmail.com>
2018-10-03 18:23:16 +0000

    strlen recursion

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: strlen
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
+}
