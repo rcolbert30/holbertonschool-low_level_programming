f08ed487c0e992e315ba3b3eaf3fd053642c394f
shaadibo <colbertrashaad@gmail.com>
2018-10-10 04:54:26 +0000

    strdup

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,26 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- *  _strlen- function that finds length of a string
- * @str: string
- *Return: count
- */
-
-int _strlen(char *str)
-{
-	int c;
-
-	c = 0;
-
-	while (str != 0)
-	{
-		c++;
-		str++;
-	}
-	return (c);
-}
 /**
  * _strdup -returns a pointer to a newly allocated space in memory
  *which contains a copy of the string given as a parameter
@@ -31,23 +11,21 @@ int _strlen(char *str)
 char *_strdup(char *str)
 {
 	char *d;
-	unsigned int i;
+	int k, m;
 
-	if (str == NULL)
+	if (str == 0)
 	{
 		return (NULL);
 	}
-	d = malloc((_strlen(str) + 1) * sizeof(char));
-	if (d == NULL)
+	for (k = 0; str[k] != '\0'; k++)
+		;
+	k++;
+	d = malloc(k  * sizeof(*d));
+	if (d == 0)
 	{
 		return (NULL);
 	}
-	i = 0;
-	while (str[i] != '\0')
-	{
-		d[i] = str[i];
-		i++;
-	}
-	d[i] = '\0';
+	for (m = 0; m < k; m++)
+		d[m] = str[m];
 	return (d);
 }
