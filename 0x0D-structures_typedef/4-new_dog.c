7d8fd4b60864acb0187e5f8eac17977a6d3da268
shaadibo <colbertrashaad@gmail.com>
2018-10-15 23:40:20 +0000

    makes a new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -23,21 +23,60 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	new_dog->name = malloc(sizeof(char) * _strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->name, name);
-	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	_strcpy(new_dog->name, name);
+	new_dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->owner, owner);
+	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 	return (new_dog);
 }
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - finds length of string
+ *@s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		;
+	}
+	return (k);
+}
+
+/**
+ *_strcpy - copies the length of a string
+ *@src: string
+ *@dest: copy destination
+ *Return: Alwasy 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int k, d;
+
+	for (d = 0; src[d] != '\0'; d++)
+		;
+	for (k = 0; k <= d; k++)
+	{
+		dest[k] = src[k];
+	}
+	return (dest);
+}
+
