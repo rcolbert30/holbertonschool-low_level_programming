3cde8d0201beea75a008eb80b21bd4a8fb26d683
shaadibo <colbertrashaad@gmail.com>
2018-10-26 18:07:15 +0000

    add node at end(pld)

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -28,11 +28,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *ptr;
 
 	if (!head)
-		return (NULL);
+		exit(1);
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
-		return (NULL);
+		exit(1);
 
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
