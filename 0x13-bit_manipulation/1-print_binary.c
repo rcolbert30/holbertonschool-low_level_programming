f5b36cc6fa2cb248342f890f7a02e6f6d39be159
shaadibo <colbertrashaad@gmail.com>
2018-11-01 20:44:28 +0000

    print binary

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary -prints the binary representation of a number
+ * @n: number to manipulate
+ * Return: binary
+*/
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		putchar('1');
+	}
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		putchar ((n & 1) + '0');
+	}
+}
