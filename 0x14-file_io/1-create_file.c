0afe3ce79a2f2c7fb378b471a72f659f896b7b03
shaadibo <colbertrashaad@gmail.com>
2018-11-07 04:27:00 +0000

    creats a file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include <sys/types.h>
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * create_file - function that creates a file
+ * @filename: name of file
+ * @text_content: string to write to the file
+ * Return: 0
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int kd, z;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = " "; 
+
+	z = open (filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (z == -1)
+		return (-1);
+	for (kd = 0; text_content[kd] != '\0'; z++)
+		;
+	if (write (z, text_content, kd) >= 0)
+	{    
+		close (z);
+		return (1);
+	}
+	else
+		return (-1);
+}
