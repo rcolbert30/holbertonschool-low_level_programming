979a515a8caacc89e8f9d7385c699d6402755228
shaadibo <colbertrashaad@gmail.com>
2018-11-07 05:51:28 +0000

    creates a file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,16 +20,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		text_content = " "; 
+		text_content = " ";
 
-	z = open (filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	z = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (z == -1)
 		return (-1);
 	for (kd = 0; text_content[kd] != '\0'; z++)
 		;
-	if (write (z, text_content, kd) >= 0)
-	{    
-		close (z);
+	if (write(z, text_content, kd) >= 0)
+	{
+		close(z);
 		return (1);
 	}
 	else
