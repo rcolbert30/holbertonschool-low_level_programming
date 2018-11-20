03734734efc834eb912a20bb0668bd413b8cd672
shaadibo <colbertrashaad@gmail.com>
2018-11-20 20:30:40 +0000

    making corresctions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = " ";
 
 	z = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
