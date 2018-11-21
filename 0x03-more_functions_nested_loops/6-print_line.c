2c3ee1a804de1b1f1431d11a31f0897270985b64
shaadibo <colbertrashaad@gmail.com>
2018-11-21 17:41:12 +0000

    print line

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,8 @@ void print_line(int n)
 {
 	int a;
 	
-	if (n == 0 || n < 0)
-		_putchar('\n');
-	for (a = 0; a <= n; a++)
-	{
+	for (a = 0; a < n; a++)
 		_putchar('_');
-	}
 	_putchar('\n');
 }
+
