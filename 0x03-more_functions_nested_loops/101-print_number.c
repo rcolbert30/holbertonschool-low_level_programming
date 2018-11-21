cd537e86db98d5338045b181232e9e3d691d2a90
shaadibo <colbertrashaad@gmail.com>
2018-11-21 18:02:22 +0000

    print number

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,8 +7,13 @@
 */
 void print_number(int n)
 {
-	if (n >= 0 && n <= 1025)
+	if (n < 0)
 	{
-		_putchar(n + '0');
+		_putchar('-');
+		n = -n;
 	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
 }
