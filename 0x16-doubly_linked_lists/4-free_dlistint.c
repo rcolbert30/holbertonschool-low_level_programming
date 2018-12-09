c40909348e79b75376ef76dced260fa010547f42
shaadibo <colbertrashaad@gmail.com>
2018-12-09 21:20:51 +0000

    freeing lists

---
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/** 
+ * free_dlistint - frees a doubly linked list
+ * @head: head of a doubly linked list
+ * Return: 0
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+}
