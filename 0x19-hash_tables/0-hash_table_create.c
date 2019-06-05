ef8a531415eb8cc42c6783a42dbdbf5cc4581e9a
shaadibo <colbertrashaad@gmail.com>
2019-06-05 00:31:11 +0000

    create

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -3,25 +3,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ht = NULL;
 
-	if (size < 1) {
+	if (size < 1) 
+	{
 		return NULL;
 	}
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL) {
+	if (ht == NULL) 
+	{
 		return (NULL);
 	}
-
-	ht->array = (hash_node_t**)malloc(size * sizeof(hash_node_t));
-	if (ht->array == NULL) {
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t));
+	if (ht->array == NULL) 
+	{
+		free(ht);
 		return (NULL);
 	}
 
 	memset(ht->array, 0, size * sizeof(hash_node_t));
-
-	ht->size = size;
-
 	return ht;
 }
