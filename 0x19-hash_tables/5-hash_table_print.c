5b8cc90ad6e2a02fb6e7a9861cb86a07fdbe4769
shaadibo <colbertrashaad@gmail.com>
2019-06-05 05:04:42 +0000

    print hash table

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -5,29 +5,28 @@
  * @ht: pointer to the hash table
  * Return: none
 */
-
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *temp = NULL;
-	unsigned long int i;
+	hash_node_t *temp;
+	int flag = 0;
+	unsigned long int i = 0;
 
 
 	if (ht == NULL)
 		return;
-	putchar('{');
+	printf("{");
 	while (i < ht->size)
 	{
-			temp = ht->array[i];
-			while (temp != NULL)
-			{
-				if (flag == 1)
-					print(", ");
-				printf("'%s': '%s'", temp->key, temp->value);
-				flag = 1;
-				temp = temp->next;
-			}
-			i++;
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
 		}
+		i++;
 	}
 	printf("}\n");
 }
