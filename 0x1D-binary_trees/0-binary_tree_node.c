38c57d2e39c4dda4457b9f8d9c35e3a8d5290a6c
shaadibo <colbertrashaad@gmail.com>
2019-07-26 19:35:15 +0000

    binary tree node

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - creates a binary tree node
+ * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  * Return: pointer to the new node, or NULL on failure
