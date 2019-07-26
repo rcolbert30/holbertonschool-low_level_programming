f8eff22749a9ad5f3acffc1a92dba1688c271903
shaadibo <colbertrashaad@gmail.com>
2019-07-26 19:46:47 +0000

    binary trees

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -5,7 +5,7 @@
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  * Return: pointer to the new node, or NULL on failure
-*/ 
+*/
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
